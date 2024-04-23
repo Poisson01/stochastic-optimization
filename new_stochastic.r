8c15858d0812949a2c492a76c35001d6c0cc91a7
Poisson01 <houg967@gmail.com>
2024-04-23 19:57:12 +0900

    dd

--- new_stochastic.r
+++ new_stochastic.r
@@ -9,7 +9,6 @@ TNR_arr <- matrix(0,times,3)
 TPR_arr <- matrix(0,times,3)
 TNR_denominator <- p
 TPR_denominator <- 0
-model_selected_arr <- c()
 for(aa in 1:times){
   set.seed(1000 + aa)
   corr_x <- matrix(rep(0,p*p),p,p)
@@ -142,49 +141,37 @@ for(aa in 1:times){
   TNR_arr[aa,2] <- BIC1st_TNR_numerator / TNR_denominator
 
   # BIC 2nd in paper
-  M_hat <- sort(M_hat); min_M <- M_hat; M <- c(); BIC_M <- 0; temp <- 2
+  M_hat <- sort(M_hat); min_M <- M_hat; M <- c(); BIC_M <- 0; temp <- 20
   M_hat_beta <- solve(t(x[,M_hat]) %*% x[,M_hat]) %*% t(x[,M_hat]) %*% y
   M_hat_sigma <- (t(y - x[,M_hat] %*% M_hat_beta) %*% (y - x[,M_hat] %*% M_hat_beta)) / n
-  visited_BIC <- c()
-  model_selected <- FALSE
-  eigen(t(x[,M_hat])%*%x[,M_hat])
-  M_hat_BIC <- -2 * sum(dnorm(y, x[,M_hat] %*% M_hat_beta, sqrt(M_hat_sigma), log = TRUE)) #+ (length(M_hat)) * log(n)
+  L_0 <- min(eigen(t(x[,M_hat])%*%x[,M_hat])$values)
+  beta_02 <- min(M_hat_beta^2)
+  cut_off <- n * L_0 * beta_02 / 2 / M_hat_sigma
+  M_hat_BIC_a <- -2 * sum(dnorm(y, x[,M_hat] %*% M_hat_beta, sqrt(M_hat_sigma), log = TRUE)) + (length(M_hat)) * log(n)
   #visited_s <- matrix(0,m,p)
   s <- c(1)
   s_beta_hat <- (solve(t(x[,s]) %*% x[,s])) %*% t(x[,s]) %*% y
-  s_sigma_hat <- (t(y - x[,s] %*% s_beta_hat) %*% (y - x[,s] %*% s_beta_hat)) / n
-  s_BIC <-  -2 * sum(dnorm(y, x[,s] %*% s_beta_hat, sqrt(s_sigma_hat), log = TRUE)) #+ (length(s)) * log(n)
-  s_fit <- lm(y ~ x[,s] - 1)
-  s_EBIC <- BIC(s_fit) + 2 * lchoose(p, length(s) + 1)
+  s_BIC <-  -2 * sum(dnorm(y, x[,s] %*% s_beta_hat, sqrt(M_hat_sigma), log = TRUE)) + (length(s)) * log(n)
+  visited_BIC <- c()
   for(iter in 1:m){
     for(i in M_hat[-1]){
       plus_s <- rep(FALSE, p); minus_s <- rep(FALSE, p)
       plus_s[s] <- TRUE; minus_s[s] <- TRUE
       if(plus_s[i] == TRUE){
         BIC_plus <- s_BIC
-        EBIC_plus <- s_EBIC
         minus_s[i] <- FALSE
         minus_beta_hat <- (solve(t(x[,minus_s]) %*% x[,minus_s])) %*% t(x[,minus_s]) %*% y
-        minus_sigma_hat <- (t(y - x[,minus_s] %*% minus_beta_hat) %*% (y - x[,minus_s] %*% minus_beta_hat)) / n
-        BIC_minus <-  -2 * sum(dnorm(y, x[,minus_s] %*% minus_beta_hat, sqrt(minus_sigma_hat), log = TRUE)) #+ (sum(minus_s)) * log(n)
-        minus_fit <- lm(y ~ x[,minus_s] - 1)
-        EBIC_minus <- BIC(minus_fit) + 2 * lchoose(p, sum(minus_s) + 1)
+        BIC_minus <-  -2 * sum(dnorm(y, x[,minus_s] %*% minus_beta_hat, sqrt(M_hat_sigma), log = TRUE)) + (sum(minus_s)) * log(n)
         visited_BIC <- append(visited_BIC,BIC_minus)
       }else {
         BIC_minus <- s_BIC
-        EBIC_minus <- s_EBIC
         plus_s[i] <- TRUE
         plus_beta_hat <- (solve(t(x[,plus_s]) %*% x[,plus_s])) %*% t(x[,plus_s]) %*% y
-        plus_sigma_hat <- (t(y - x[,plus_s] %*% plus_beta_hat) %*% (y - x[,plus_s] %*% plus_beta_hat)) / n
-        BIC_plus <-  -2 * sum(dnorm(y, x[,plus_s] %*% plus_beta_hat, sqrt(plus_sigma_hat), log = TRUE)) #+ (sum(plus_s)) * log(n)
-        plus_fit <- lm(y ~ x[,plus_s] - 1)
-        EBIC_plus <- BIC(plus_fit) + 2 * lchoose(p, sum(plus_s) + 1)
+        BIC_plus <-  -2 * sum(dnorm(y, x[,plus_s] %*% plus_beta_hat, sqrt(M_hat_sigma), log = TRUE)) + (sum(plus_s)) * log(n)
         visited_BIC <- append(visited_BIC,BIC_plus)
       }
       if(sum(plus_s) <= n^(2/3)){
-        p_s <- (length(M_hat) - sum(plus_s))
-        plus_cut_off <- n * 2 #2 * p_s * (log(p)) #+ 2 * p_s * log(log(p)) + p_s
-        if(abs(M_hat_BIC - BIC_plus) <= plus_cut_off){
+        if(abs(M_hat_BIC - BIC_plus) <= cut_off){
           M <- all_index[plus_s]
           BIC_M <- BIC_plus
           if(length(M) < length(min_M)){
@@ -193,9 +180,7 @@ for(aa in 1:times){
           }
         }
       }
-      m_s <- (length(M_hat) - sum(minus_s))
-      minus_cut_off <- n * 2 #2 * m_s * (log(p)) #+ 2 * m_s * log(log(p)) + m_s
-      if(abs(M_hat_BIC - BIC_minus) <= minus_cut_off){
+      if(abs(M_hat_BIC - BIC_minus) <= cut_off){
         M <- all_index[minus_s]
         BIC_M <- BIC_minus
         if(length(M) < length(min_M)){
@@ -203,27 +188,25 @@ for(aa in 1:times){
           min_BIC_M <- BIC_M
         }
       }
-      prob <- 1/(1 + exp((-0.5 / temp * EBIC_minus) + (0.5 / temp * EBIC_plus)))
+      prob <- 1/(1 + exp((-0.5 / temp * BIC_minus) + (0.5 / temp * BIC_plus)))
       z <- rbinom(1, 1, prob)
       if((sum(plus_s) > n ^ (2 / 3)) & (z == 1)){
 
       }else {
         if(z == 1){
           s <- all_index[plus_s]
-          s_EBIC <- EBIC_plus
           s_BIC <- BIC_plus
         }else {
             s <- all_index[minus_s]
-            s_EBIC <- EBIC_minus
             s_BIC <- BIC_minus
         }
       }
       #print(s)
-      if (identical(as.numeric(s),c(1,2,3,4,5,6,7))) {
-        model_selected <- TRUE
-      }
+      #visited_s[iter,s] <- 1
+      #if (identical(as.numeric(s),c(1,2,3,4,5,6,7))) {
+      #  model_selected <- TRUE
+      #}
     }
-    model_selected_arr <- append(model_selected_arr,model_selected)
     #if(iter %% 100 == 0){
     #  cat("BIC 2nd",iter,"\n")
     #}
